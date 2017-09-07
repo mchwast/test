16692dd706bcf524a2fab62c20edc26827ffea8c
mchwast <mchwast@gmail.com>
2017-09-07 23:00:36 +0200

    1

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -3,11 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-//pole 44 online ma zostac 2bc0
+//pole 44 online ma zostac 2bc01
 namespace ConsoleApp1
 {//66
     //8
-    class Programlocal zostaje poprawionybder
+    class Programlocal zostaje poprawionybder1
     {
         static void Main(string[] args)
         {
