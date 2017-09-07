ac3ecda1300e9d0681ab15ade362fd5783849fd9
mchwast <mchwast@gmail.com>
2017-09-07 22:06:34 +0200

    zmna program 12 pole 1

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -3,11 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+//pole 1
 namespace ConsoleApp1
 {//66
     //8
-    class Program
+    class Program12
     {
         static void Main(string[] args)
         {
